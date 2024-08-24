d8b66cee46116c4ef8860cb01df4b4d7b870c418
NirojEdulab <niroj@edulab.in>
2024-08-24 22:23:02 +0530

    changed profile cover image

--- src/components/ProfileBanner.jsx
+++ src/components/ProfileBanner.jsx
@@ -49,7 +49,7 @@ const ProfileBanner = () => {
             loading="lazy"
             alt={`${userProfileData.username} cover picture`}
             onError={(e) => {
-              e.target.src = `https://avatar.iran.liara.run/username?username=${userProfileData.username}`;
+              e.target.src = `https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80`;
             }}
           />
         </div>
